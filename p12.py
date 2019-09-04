41ccdf0d40d620aa9a65fffbba75708ba973f4a0
anu-19cloud <54556752+anu-19cloud@users.noreply.github.com>
2019-09-04 16:15:41 -0400

    Update p12.py

--- p12.py
+++ p12.py
@@ -8,5 +8,5 @@ sc = slack.WebClient(token)
 print((sc.api_call("api.test"))) 
 
 
-get_info = sc.api_call("conversations.history", channel ="G0L0V05GF")
+get_info = sc.api_call("conversations.history", channel ="XXXXXXX")
 print(type(get_info)) 
