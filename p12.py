5ea97448acec51b88865d64265b1ea0ab0220463
anu-19cloud <54556752+anu-19cloud@users.noreply.github.com>
2019-09-05 10:01:33 -0400

    Update p12.py

--- p12.py
+++ p12.py
@@ -3,10 +3,19 @@ import slack
 
 token = os.environ.get("SLACK_TOKEN")
 sc = slack.WebClient(token)
+messages = []
 
+#print(sc.api_call("auth.test")) 
 
-print((sc.api_call("api.test"))) 
+get_info = sc.conversations_history (channel ="XXXXXXX", limit=100, default=0)
+#print(get_info)
+messages.extend(get_info['messages'])
+#messages.sort(key = lambda message: message['ts'])
+#print(messages)
+response = ""
+with open("out.txt",'w') as f:
+  for msg in messages:
+    response = 'UserId:' + msg['user']+' , Timestamp :' + msg['ts']+' , Text : msg['text']
+    print(response)
+    f.write(response)
 
-
-get_info = sc.api_call("conversations.history", channel ="XXXXXXX", limit=100, default=0)
-print(get_info)
